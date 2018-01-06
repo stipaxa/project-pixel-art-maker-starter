9c1d9dd015bd97ef6c628faa53038107c44a7cc6
stipaxa <35047495+stipaxa@users.noreply.github.com>
2018-01-06 00:58:33 +0000

    Add files via upload
    Make up the grid.

--- designs.js
+++ designs.js
@@ -1,43 +1,39 @@
 $( document ).ready(function() {
 $("#sizePicker").submit(function() {
-    //alert("submitted");
     // Select size input
-    var grid_height  = $('#input_height').val();
-    var grid_width   = $('#input_width').val();
-    $("#span1").text("grid_height: " + grid_height).show();
-    $("#span2").text("grid_width: " + grid_width).show();
+    var grid_height  = $("#input_height").val();
+    var grid_width   = $("#input_width").val();
+    //$("#span1").text("grid_height: " + grid_height).show();
+    //$("#span2").text("grid_width: " + grid_width).show();
 
-    makeGrid(grid_width, grid_height);
+    drawGrid(grid_width, grid_height);
+    styleGrid();
 
     event.preventDefault();
 });
 });
 
-// When size is submitted by the user, call makeGrid()
-function makeGrid(w, h) {
+// Make up the grid
+function styleGrid() {
 	$("#pixel_canvas").css("border-collapse", "collapse");
-	$("#pixel_canvas tr").css("border", "1px solid black");
-	$("#pixel_canvas td").css("border", "1px solid black");		
-	$("pixel_canvas").css("background-color", "#f5f5f5");
-	//$("pixel_canvas td").css("background-color", "yellow");
+	$("#pixel_canvas").css("margin","0 auto");
 
+	$("#pixel_canvas td").css("border", "1px solid black");
+	$("#pixel_canvas td").css("width", "10px");
+	$("#pixel_canvas td").css("height", "10px");
+	$("#pixel_canvas td").css("padding", "0px");
+	$("#pixel_canvas td").css("background-color", "#ffff99");
+}
+
+// Draw the grid
+function drawGrid(w, h) {
+	$(".pixelTable").remove();
 	for (var i = 0; i < h; i++) {	
-		$("#pixel_canvas").append("<tr class='trClass'></tr>");
+		$("#pixel_canvas").append("<tr class='pixelTable'></tr>");
 	}
 
 	$("#pixel_canvas tr").each(function() {
 			for (var j = 0; j < w; j++) 
-				$(this).append("<td class='tdClass'>blaaaa</td>");
-	});
-
-	/*
-	for (var i = 0; i < h*0; i++) {
-		$("#pixel_canvas").append("<tr class='trClass'></tr>");
-
-		$("#pixel_canvas tr").each(function() {
-			for (var j = 0; j < w; j++) 
-				$(this).append("<td class='tdClass'>blaaaa</td>");
-			}
-		});
-	}*/
+				$(this).append("<td class='pixelTable'></td>");
+	});	
 }
