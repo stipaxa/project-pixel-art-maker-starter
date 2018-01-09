986484dba8301334fff5a144285a0eef225f695f
stipaxa <35047495+stipaxa@users.noreply.github.com>
2018-01-09 22:36:24 +0000

    Add files via upload
    Second step of coloring cell (Click function).

--- designs.js
+++ designs.js
@@ -1,48 +1,44 @@
-
 $( document ).ready(function() {
 $("#sizePicker").submit(function() {
     // Select size input
     var grid_height  = $("#input_height").val();
     var grid_width   = $("#input_width").val();
-    //$("#span1").text("grid_height: " + grid_height).show();
-    //$("#span2").text("grid_width: " + grid_width).show();
 
     drawGrid(grid_width, grid_height);
     styleGrid();
-
+    
     event.preventDefault();
 });
 });
 
 // Make up the grid
 function styleGrid() {
-	$("#pixel_canvas").css("border-collapse", "collapse");
-	$("#pixel_canvas").css("margin","0 auto");
+    $("#pixel_canvas").css("border-collapse", "collapse");
+    $("#pixel_canvas").css("margin","0 auto");
 
-	$("#pixel_canvas td").css("border", "1px solid black");
-	$("#pixel_canvas td").css("width", "20px");
-	$("#pixel_canvas td").css("height", "20px");
-	$("#pixel_canvas td").css("padding", "0px");
-	$("#pixel_canvas td").css("background-color", "#ffff99");
+    $("#pixel_canvas td").css("border", "1px solid black");
+    $("#pixel_canvas td").css("width", "20px");
+    $("#pixel_canvas td").css("height", "20px");
+    $("#pixel_canvas td").css("padding", "0px");
+    $("#pixel_canvas td").css("background-color", "#ffff99");
 
-	$(".pixelTable").click(function(event){
-		var target = $(event.target);
-		if(target.is("td")) {
-			target.css("background-color", "red");			
-		}
-	});
+    $(".pixelTable").click(function(event){
+        var target = $(event.target);
+        if(target.is("td")) {
+            target.css("background-color", $("#colorPicker").val());
+        } 
+    });
 }
 
 // Draw the grid
 function drawGrid(w, h) {
-	$(".pixelTable").remove();
-	for (var i = 0; i < h; i++) {	
-		$("#pixel_canvas").append("<tr class='pixelTable'></tr>");
-		//$("#pixel_canvas").append("<tr></tr>");
-	}
+    $(".pixelTable").remove();
+    for (var i = 0; i < h; i++) {    
+        $("#pixel_canvas").append("<tr class='pixelTable'></tr>");
+    }
 
-	$("#pixel_canvas tr").each(function() {
-			for (var j = 0; j < w; j++) 
-				$(this).append("<td class='pixelTable'></td>");
-	});	
+    $("#pixel_canvas tr").each(function() {
+        for (var j = 0; j < w; j++) 
+            $(this).append("<td class='pixelTable'></td>");
+    });    
 }
