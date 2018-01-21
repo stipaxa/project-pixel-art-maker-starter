8b9100177c64d46f45a57eaa80c7152745a71162
stipaxa <35047495+stipaxa@users.noreply.github.com>
2018-01-21 21:24:09 +0000

    Add files via upload
    Optimization of code.

--- designs.js
+++ designs.js
@@ -1,46 +1,31 @@
-$("#sizePicker").submit(function() {
-    // Select size input
+$("#size_picker").submit(function() {
     var grid_height  = $("#input_height").val();
     var grid_width   = $("#input_width").val();
 
-    drawGrid(grid_width, grid_height);
-    styleGrid();
-    
-    event.preventDefault();
-});
-
-// Make up the grid
-function styleGrid(event) {
-    $(".pixelTable").click(function(event){
-        var target = $(event.target);
-        if(target.is("td")) {
-            target.css("background-color", $("#colorPicker").val());
-        } 
-    });
-
-    $(".pixelTable").mousedown(function(event) {
-        $(".pixelTable").mousemove(function(event) {
-            var target = $(event.target);
-            if(target.is("td")) {
-                target.css("background-color", $("#colorPicker").val());
-            } 
-            event.preventDefault();
-        })
-    }).mouseup(function(event) {
-        $(".pixelTable").unbind("mousemove");
-        event.preventDefault();
-    });
-}
-
-// Draw the grid
-function drawGrid(w, h) {
-    $(".pixelTable").remove();
-    for (var i = 0; i < h; i++) {    
-        $("#pixel_canvas").append("<tr class='pixelTable'></tr>");
+    $("#pixel_canvas tr").remove();
+    for (var i = 0; i < grid_height; i++) {    
+        $("#pixel_canvas").append("<tr></tr>");
     }
 
     $("#pixel_canvas tr").each(function() {
-        for (var j = 0; j < w; j++) 
-            $(this).append("<td class='pixelTable'></td>");
+        for (var j = 0; j < grid_width; j++) 
+            $(this).append("<td></td>");
     });   
-}
+
+    $("#pixel_canvas td").click(function(event){
+        $(event.target).css("background-color", $("#color_picker").val());
+        event.preventDefault(event);
+    });
+
+    $("#pixel_canvas td").mousedown(function(event) {
+        $("#pixel_canvas td").mousemove(function(event) {
+            $(event.target).css("background-color", $("#color_picker").val());
+            event.preventDefault(event);
+        })
+    }).mouseup(function(event) {
+        $("#pixel_canvas td").unbind("mousemove");
+        event.preventDefault(event);
+    });
+    
+    event.preventDefault(event);
+});
