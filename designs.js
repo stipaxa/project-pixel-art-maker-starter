e863490b7ad4becb1b9ad99a9d1a29b8cda154c6
stipaxa <35047495+stipaxa@users.noreply.github.com>
2018-01-26 21:20:40 +0000

    Add files via upload
    Add comments and made optimization of selector $("#pixel_canvas td").

--- designs.js
+++ designs.js
@@ -1,31 +1,37 @@
 $("#size_picker").submit(function(event) {
-    var grid_height  = $("#input_height").val();
-    var grid_width   = $("#input_width").val();
+    let grid_height = $("#input_height").val();
+    let grid_width  = $("#input_width").val();
 
+    // Remove old table, create new
     $("#pixel_canvas tr").remove();
-    for (var i = 0; i < grid_height; i++) {    
+    for (let i = 0; i < grid_height; i++) {    
         $("#pixel_canvas").append("<tr></tr>");
     }
-
     $("#pixel_canvas tr").each(function() {
-        for (var j = 0; j < grid_width; j++) 
+        for (let j = 0; j < grid_width; j++) 
             $(this).append("<td></td>");
-    });   
+    });
 
-    $("#pixel_canvas td").click(function(event){
+    // Optimization: Select once, use many times
+    let canvas_td = $("#pixel_canvas td");
+
+    // Single click paint
+    canvas_td.click(function(event){
         $(event.target).css("background-color", $("#color_picker").val());
         event.preventDefault();
     });
 
-    $("#pixel_canvas td").mousedown(function(event) {
-        $("#pixel_canvas td").mousemove(function(event) {
+    // Drag and draw paint    
+    canvas_td.mousedown(function(event) {
+        canvas_td.mousemove(function(event) {
             $(event.target).css("background-color", $("#color_picker").val());
             event.preventDefault();
         })
     }).mouseup(function(event) {
-        $("#pixel_canvas td").unbind("mousemove");
+        canvas_td.unbind("mousemove");
         event.preventDefault();
     });
     
+    // Disable page reloading
     event.preventDefault();
 });
